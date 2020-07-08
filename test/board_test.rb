6d5eda2edcc80815fe59222043d9e718ddff0dfc
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-08 17:03:15 -0600

    Last Push before pulling from main

--- test/board_test.rb
+++ test/board_test.rb
@@ -5,7 +5,6 @@ require './lib/cell'
 require './lib/board'
 
 
-
 class BoardTest < Minitest::Test
 
     def test_it_exists
@@ -18,8 +17,6 @@ class BoardTest < Minitest::Test
       # skip
       board1 = Board.new
 
-
-
       assert_equal board1.cells.length, 16
     end
 
@@ -31,44 +28,43 @@ class BoardTest < Minitest::Test
       assert_equal board1.valid_coordinate?("A2"), true
     end
 
-    def test_it_can_show_valid_coordinates
+    def test_it_can_tell_valid_placement
       skip
       board1 = Board.new
       ship1 = Ship.new("Challenger", 5)
 
-
       assert_equal board1.valid_placement?(challenger, ["A1, A2"]), false
     end
 
-    def test_it_can_be_hit
-      skip
-      ship1 = Ship.new("Carrier", 5)
-
-      ship1.hit
-
-
-      assert_equal 4, ship1.health
-    end
-
-    def test_it_can_sink
-      skip
-      ship1 = Ship.new("Battleship", 4)
-
-      ship1.hit
-      ship1.hit
-      ship1.hit
-      ship1.hit
-
-      assert_equal true, ship1.sunk?
-    end
-
-    def test_it_can_make_multiple_ships
-      skip
-      ship1 = Ship.new("Battleship", 4)
-      ship2 = Ship.new("Cruiser", 3)
-
-      assert_equal "Battleship", ship1.name
-      assert_equal "Cruiser", ship2.name
-    end
+    # def test_it_can_be_hit
+    #   skip
+    #   ship1 = Ship.new("Carrier", 5)
+    #
+    #   ship1.hit
+    #
+    #
+    #   assert_equal 4, ship1.health
+    # end
+    #
+    # def test_it_can_sink
+    #   skip
+    #   ship1 = Ship.new("Battleship", 4)
+    #
+    #   ship1.hit
+    #   ship1.hit
+    #   ship1.hit
+    #   ship1.hit
+    #
+    #   assert_equal true, ship1.sunk?
+    # end
+    #
+    # def test_it_can_make_multiple_ships
+    #   skip
+    #   ship1 = Ship.new("Battleship", 4)
+    #   ship2 = Ship.new("Cruiser", 3)
+    #
+    #   assert_equal "Battleship", ship1.name
+    #   assert_equal "Cruiser", ship2.name
+    # end
 
   end
