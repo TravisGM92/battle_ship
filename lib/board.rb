a9cb33dc5504bc5d688a7a0cb883f6e41dd989bd
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-08 18:34:59 -0600

    Got Board.rb working again

--- lib/board.rb
+++ lib/board.rb
@@ -4,7 +4,7 @@ require './lib/ship'
 
 class Board
 
-  attr_accessor :row_a, :row_b, :row_b, :row_d, :a1, :a2, :a3, :a4, :b1, :b2, :b3, :b4, :c1, :c2, :c3, :c4, :d1, :d2, :d3, :d4,
+  attr_accessor :row_a, :row_b, :row_b, :row_d, :a1, :a2, :a3, :a4, :b1, :b2, :b3, :b4, :c1, :c2, :c3, :c4, :d1, :d2, :d3, :d4
 
   def initialize
     @a1 = Cell.new("A1")
@@ -99,13 +99,13 @@ class Board
 
     # Checks if ship length is equal to coordinate length, then checks if numbers in coordinates are consecutive. After, it checks if all letters are identical
     if ship.length == coordinate.length && colum_numbers.collect{ |numbs| colum_numbers[0] == numbs} && index_of_letters_of_coordinates.all?{ |numb| numb == index_of_letters_of_coordinates[0]}
-      true
+      "true1"
     # Checks if ship length is equal to coordinate length, then checks if the letters of the coordinates are consecutive, lastly checks if coordinates have 2 elements
     elsif ship.length == coordinate.length && index_of_letters_of_coordinates[0] + 1 == index_of_letters_of_coordinates[1] && coordinate.length == 2 && colum_numbers.all?{ |numbs| colum_numbers[0] == numbs}
-      true
+      "true2"
     # Checks if ship length is equal to coordinate length, then checks if the letters of the coordinates are consecutive, lastly checks if coordinates have 3 elements
     elsif ship.length == coordinate.length && index_of_letters_of_coordinates[0] + 1 == index_of_letters_of_coordinates[1] && index_of_letters_of_coordinates[1] + 1 == index_of_letters_of_coordinates[2] && coordinate.length == 3 && colum_numbers.all?{ |numbs| colum_numbers[0] == numbs}
-      true
+      "true3"
     elsif coordinate.length == 0
       false
     elsif coordinate.any?{ |pair| not cells[pair].empty?}
@@ -128,21 +128,7 @@ class Board
     end
   end
 
-  # def render(show=false)
-  #   if cell.fired > 0 && cell.cell.empty? && show == false
-  #     "M"
-  #   elsif cell.fired > 0 && cell.cell.empty? == false && show == false && cell.ship.health > 0
-  #     "H"
-  #   elsif cell.fired == 0 && cell.cell.empty? && show == false
-  #     "."
-  #   elsif cell.fired == 0 && cell.cell.empty? == false && show == true
-  #     "S"
-  #   elsif cell.cell.empty? == false && cell.ship.health == 0 && show == false
-  #     "X"
-  #   elsif show == true
-  #     "S"
-  #   end
-  # end
+
 
 
 end
@@ -155,13 +141,11 @@ board1.place(ship2, ["A1", "A2", "A3"])
 cell_1 = board1.cells["A1"]
 cell_2 = board1.cells["A2"]
 cell_3 = board1.cells["A3"]
-#
 
-# p board1.render
-#
+
 # p board1.valid_placement?(ship2, ["A1", "A2", "A3"])
 p board1.valid_placement?(ship1, ["A1", "B2"])
-p ship1
+# p ship1
 # p ["A1", "A2", "A3"].any?{ |pair| not board1.cells[pair].empty?}
 
 # require "pry"; binding.pry
