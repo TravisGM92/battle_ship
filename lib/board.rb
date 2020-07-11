55745c6533a0232ee80498d54a954fa64da991d0
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-11 17:00:24 -0600

    committing to merge

--- lib/board.rb
+++ lib/board.rb
@@ -7,7 +7,8 @@ class Board
   attr_accessor :row_a, :row_b, :row_c,:row_d,
                 :a1, :a2, :a3, :a4,
                 :b1, :b2, :b3, :b4,
-                :c1, :c2, :c3, :c4, :d1, :d2, :d3, :d4
+                :c1, :c2, :c3, :c4,
+                :d1, :d2, :d3, :d4
 
   def initialize
     @a1 = Cell.new("A1")
