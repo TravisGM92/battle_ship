27bec579067e5e69a7424ffa4497f89d4d370f43
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-13 19:48:08 -0600

    Create smart computer test file

--- test/smart_computer_test.rb
+++ test/smart_computer_test.rb
@@ -1,10 +1,6 @@
+require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/cell'
 require './lib/ship'
 require './lib/game'
 require './lib/board'
-
-class Smart
-
-  def initialize
-  end
-end
